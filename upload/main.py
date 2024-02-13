24dab54b09efd20b7502ec8b0e3a20e744b4b3fb
vanessakovalsky <v.david@kovalibre.com>
2024-02-13 15:24:11 +0100

    Update main.py to make code run in cloud functions

--- upload/main.py
+++ upload/main.py
@@ -1,14 +1,18 @@
+import functions_framework
+
 from urllib.request import urlopen
 from google.cloud import storage
 
 client = storage.Client()
 
-
-def upload():
+@functions_framework.http
+def upload(request):
 
     filedata = urlopen('https://data.police.uk/data/archive/latest.zip')
     datatoupload = filedata.read()
 
     bucket = client.get_bucket('demovanessa')
-    blob = Blob("latest.zip", bucket)
+    blob = bucket.blob("latest.zip")
     blob.upload_from_string(datatoupload)
+
+    return blob.public_url
